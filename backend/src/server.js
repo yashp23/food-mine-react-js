8f122b576e170a2e7041d99ba49185356db902e1
yashp23 <yash67238@gmail.com>
2024-10-15 13:50:24 +0530

    new url update

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ['https://food-mine-react-js-usa1.vercel.app/'],
+    origin: ['https://food-mine-react-js-4e3z.vercel.app/'],
   })
 );
 
